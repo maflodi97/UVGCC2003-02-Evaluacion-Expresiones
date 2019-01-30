79b8b1c0b21964a3579b8bfafe21e5a034e7ecd5
maflodi97 <42972970+maflodi97@users.noreply.github.com>
2019-01-30 00:06:21 -0600

    Modificaciones y comentarios "pre y post"

--- EvaluadorPostfix/src/Main/Pila.java
+++ EvaluadorPostfix/src/Main/Pila.java
@@ -2,32 +2,52 @@
 
 import java.util.Vector;
 
-public class Pila implements iPila<String> {
+public class Pila <E>
+implements iPila<E> {
     //  todos estos metodos ya existen.
     //  Puse String como generic como placeholder pero ahi
     //  veremos a que lo cambiamos
 
-    Vector<String> vec = new Vector<>();
+    protected Vector<E> vec;
+    
+    public Pila() {
+    	vec = new Vector<E>();
+    }
     //  implementacion de generics aunque solo existe un ADT.
 
-    public void push(String obj){
+    @Override
+    public void push(E obj){
+    	//post: el objeto se agrega al final del Vector
+    	
         this.vec.add(obj);
     }
 
-    public String pop(){
-        if (!this.empty()){
+    @Override
+    public E pop(){
+    	// pre: el Vector no está vacío 
+    	// post: el último elemento "pusheado" es eliminado y mostrado.
+        
+    	return vec.remove( vec.size()-1)
+    	/** CREO que no es necesario el If puesto que es un "pre" el que no esté vacio el stack
+    	 * pero no sé,¿ que piensan ustedes?
+    	if (!this.empty()){
             return this.vec.get(this.vec.size() - 1);
         } else{
             return this.vec.get(0);
+            **/
         }
     }
 
-    public String peek(){
-
+	@Override
+    public E peek(){
+    	// pre: el vector no está vacío
+    	// post: devuelve el valor de hasta arriba ( el siguiente a ser eliminado por "pop()" )
         return this.vec.get(this.vec.size() - 1);
     }
 
+	@Override
     public boolean empty(){
+		// post: Retorna si True si el vector está vacío
 
         if (this.vec.isEmpty()){
             return true;
@@ -37,9 +57,13 @@ public class Pila implements iPila<String> {
     }
 
     public int size(){
+    	// post: retorna el numero de elementos que hay en el vector
         return this.vec.size();
     }
 
+    /**
+     * No entiendo que hace este metodo, o para qué lo usariamos.... :/
+     */
     public void crecer(int n){
         this.vec.setSize(n);
     }
