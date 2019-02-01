0e0f719c10c11dbcca391ae0220a9505e38690b3
Pablo Sao <pablo_sao@outlook.com>
2019-02-01 12:08:46 -0600

    .

--- EvaluadorPostfix/src/CalculadoraTest.java
+++ EvaluadorPostfix/src/CalculadoraTest.java
@@ -1,22 +1,22 @@
 //package Main;
 
-import org.junit.Test;
-
-import static org.junit.Assert.*;
+//import org.junit.Test;
+//
+//import static org.junit.Assert.*;
 
 public class CalculadoraTest {
 
-    @Test
-    public void test_para_sumar() {
-        Calculadora calc = new Calculadora();
-
-        assertTrue(calc.sumar(2.0, 2.0) == 4.0);
-    }
-
-    @Test
-    public void test_para_dividir() {
-        Calculadora calc = new Calculadora();
-
-        assertEquals(calc.dividir(1, 0), 0.0, 0.0);
-    }
+//    @Test
+//    public void test_para_sumar() {
+//        Calculadora calc = new Calculadora();
+//
+//        assertTrue(calc.sumar(2.0, 2.0) == 4.0);
+//    }
+//
+//    @Test
+//    public void test_para_dividir() {
+//        Calculadora calc = new Calculadora();
+//
+//        assertEquals(calc.dividir(1, 0), 0.0, 0.0);
+//    }
 }
--- EvaluadorPostfix/src/PilaTest.java
+++ EvaluadorPostfix/src/PilaTest.java
@@ -1,20 +1,20 @@
 //package Main;
 
-import static org.junit.Assert.*;
+//import static org.junit.Assert.*;
 
 public class PilaTest {
 
-    @org.junit.Test
-    public void test_para_stack_vacio() {
-        Pila p = new Pila();
-
-        assertTrue(p.empty());
-    }
-
-    @org.junit.Test
-    public void test_para_size() {
-        Pila p = new Pila();
-
-        assertTrue(p.size() < 1);
-    }
+//    @org.junit.Test
+//    public void test_para_stack_vacio() {
+//        Pila p = new Pila();
+//
+//        assertTrue(p.empty());
+//    }
+//
+//    @org.junit.Test
+//    public void test_para_size() {
+//        Pila p = new Pila();
+//
+//        assertTrue(p.size() < 1);
+//    }
 }
